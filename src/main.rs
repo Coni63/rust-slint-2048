50a05cd9c11f773fe8594db6a9c6335ff39a87c1
Nicolas M <mine.nicolas@gmail.com>
2024-01-15 19:44:53 +0100

    fixed the UI part

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod board;
 use std::{cell::RefCell, rc::Rc};
 
 use board::Board;
-use slint::{ModelRc, SharedString, Weak};
+use slint::{ModelRc, SharedString};
 
 slint::include_modules!();
 
@@ -18,37 +18,27 @@ fn update_app(ui: AppWindow, game: Rc<RefCell<Board>>) {
 
 fn main() -> Result<(), slint::PlatformError> {
     let game = Rc::new(RefCell::new(Board::new(32)));
-
     let ui = AppWindow::new()?;
-    let ui_handle = ui.as_weak();
 
-    update_app(ui_handle.unwrap(), Rc::clone(&game));
+    let ui_handle1 = ui.as_weak();
+    let ui_handle2 = ui.as_weak();
+    let ui_handle3 = ui.as_weak();
 
     let game_on_restart = Rc::clone(&game);
-    ui.on_restart(move |seed| {
-        println!("Restarting with seed: {}", seed);
-
-        game_on_restart.borrow_mut().restart(seed.parse().unwrap());
-
-        println!("{}", game_on_restart.borrow());
-        // let window = ui_handle.unwrap();
-
-        // update_app(window, Rc::clone(&game_on_restart));
-    });
-
     let game_on_play = Rc::clone(&game);
-    ui.on_play(move |direction| {
-        println!("Playing with direction: {:?}", direction);
 
-        game_on_play.borrow_mut().apply_action(direction as u8);
-
-        println!("{}", game_on_play.borrow());
-
-        // let window = ui_handle.unwrap();
-
-        // update_app(window, Rc::clone(&game_on_play));
+    ui.on_restart(move |seed| {
+        game_on_restart.borrow_mut().restart(seed.parse().unwrap());
+        update_app(ui_handle2.unwrap(), Rc::clone(&game_on_restart));
     });
 
+    ui.on_play(move |direction| {
+        game_on_play.borrow_mut().apply_action(direction as u8);
+        update_app(ui_handle3.unwrap(), Rc::clone(&game_on_play));
+    });
+
+    // setup and run the UI
+    update_app(ui_handle1.unwrap(), Rc::clone(&game));
     ui.run()?;
     Ok(())
 }
